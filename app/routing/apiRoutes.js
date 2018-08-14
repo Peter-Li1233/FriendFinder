375b9b82caf71f2e8f365579ba7297dbebb841d0
Peter-Li1233 <minxiangli08@gmail.com>
2018-08-14 18:24:35 -0400

    it begins to work, will add a carousel tomorrow

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,13 +1,12 @@
 // ===============================================================================
 // LOAD DATA
 // We are linking our routes to a series of "data" sources.
-// These data sources hold arrays of information on friend-data, waitinglist, etc.
+// These data sources hold arrays of information on friend-data.
 // ===============================================================================
 
 var friendData = require("../data/friend");
-//var waitListData = require("../data/waitinglistData");
 
-console.log(friendData);
+//console.log(friendData);
 
 // ===============================================================================
 // ROUTING
@@ -16,50 +15,67 @@ console.log(friendData);
 module.exports = function(app) {
   // API GET Requests
   // Below code handles when users "visit" a page.
-  // In each of the below cases when a user visits a link
-  // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
   // ---------------------------------------------------------------------------
 
   app.get("/api/friends", function(req, res) {
     res.json(friendData);
   });
 
-//   app.get("/api/waitlist", function(req, res) {
-//     res.json(waitListData);
-//   });
 
   // API POST Requests
   // Below code handles when a user submits a survey form and thus submits data to the server.
   // In each of the below cases, when a user submits form data (a JSON object)
-  // ...the JSON is pushed to the appropriate JavaScript array
-  // (ex. User fills out a reservation request... this data is then sent to the server...
-  // Then the server saves the data to the tableData array)
+  // ...the JSON is compared with friend data to find the  closest match
+  // (ex. User fills out a survey table... this data is then sent to the server...
   // ---------------------------------------------------------------------------
 
   app.post("/api/friends", function(req, res) {
-    // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
+    // Note the code here. Our "server" will respond to requests for the closest match.
     // It will do this by sending out the value "true" have a table
     // req.body is available since we're using the body-parser middleware
-    // if (tableData.length < 5) {
-        console.log(req.body);
-      friendData.push(req.body);
-      res.json(true);
-    // }
-    // else {
-    //   waitListData.push(req.body);
-    //   res.json(false);
-    // }
+
+    var guestScores = [];
+    for (i=0; i<req.body.scores.length; i++) {
+        guestScores.push(parseInt(req.body.scores[i]));
+    }
+
+    var matchArr =[];
+    for(i=0;i<friendData.length;i++) {    
+        matchArr.push(findMatch(guestScores, friendData[i].scores));
+    }
+
+    res.json(friendData[indexOfSmallest(matchArr)]);
+    
   });
 
   // ---------------------------------------------------------------------------
   // I added this below code so you could clear out the table while working with the functionality.
   // Don"t worry about it!
+  // For future choice
 
   app.post("/api/clear", function() {
     // Empty out the arrays of data
-    tableData = [];
-    waitListData = [];
+    friendData = [];
 
-    console.log(tableData);
+    console.log(friendData);
   });
+
+  function  findMatch(arr1,arr2) {
+      var x = 0;
+      for(j=0;j<arr1.length;j++) {
+        x +=  Math.abs(arr1[j] - arr2[j]);
+      }
+
+      return x;
+  }; 
+
+  function indexOfSmallest(arr) {
+      var lowest = 0;
+      for (var i = 1; i < arr.length; i++) {
+          if(arr[i] < arr[lowest]) {
+              lowest = i;
+          }
+      }
+      return lowest;
+  }
 };
