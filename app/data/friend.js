723e93164c2c986a87c722523a040c101d81277b
Peter-Li1233 <minxiangli08@gmail.com>
2018-08-14 09:40:02 -0400

    finish part of the homework

---
+++ app/data/friend.js
@@ -0,0 +1,64 @@
+var friend1 = {
+    "name": "lady Sharon",
+    "photo": "https://openphoto.net/volumes/harmX/20070522/openphotonet_mvc-025f.jpg",
+    "scores": [5,3,1,4,2,4,2,1,3,5]
+};
+
+var friend2 = {
+    "name": "lady Moore",
+    "photo": "https://openphoto.net/volumes/amanzi/20041119/openphotonet_chtfrm03.jpg",
+    "scores": [4,4,5,1,2,3,1,1,3,2]
+};
+
+var friend3 = {
+    "name": "lady Julie",
+    "photo": "https://openphoto.net/volumes/amanzi/20041119/openphotonet_sealcl03.jpg",
+    "scores": [1,2,3,5,4,4,5,3,3,1]
+};
+
+var friend4 = {
+    "name": "lady Nicole",
+    "photo": "https://openphoto.net/volumes/exedesign/20080215/openphotonet_488435693_ab8185ca25_o.jpg",
+    "scores": [1,1,1,2,4,5,1,3,4,1]
+};
+
+var friend5 = {
+    "name": "lady Bug",
+    "photo": "https://openphoto.net/volumes/miro/20131028/openphotonet_red_panda.JPG",
+    "scores": [2,2,3,5,4,1,5,2,4,3]
+};
+
+var friend6 = {
+    "name": "Han Solo",
+    "photo": "https://openphoto.net/volumes/miro/20131028/openphotonet_lionfish_1.JPG",
+    "scores": [4,5,4,5,4,5,5,4,4,5]
+};
+
+var friend7 = {
+    "name": "Yoda",
+    "photo": "https://openphoto.net/volumes/rmarinello/20071011/openphotonet_IMG_2460_resize.jpg",
+    "scores": [3,5,3,5,2,2,2,4,4,3]
+};
+
+var friend8 = {
+    "name": "James Bond",
+    "photo": "https://openphoto.net/volumes/miro/20131028/openphotonet_red_panda.JPG",
+    "scores": [5,5,5,5,5,5,5,4,4,5]
+};
+
+var friend9 = {
+    "name": "Forrest Gump",
+    "photo": "https://openphoto.net/volumes/miro/20120206/openphotonet_Prairie%20dog_resize.jpg",
+    "scores": [1,2,1,2,2,1,1,2,2,2]
+};
+
+var friend10 = {
+    "name": "Harry Potter",
+    "photo": "https://openphoto.net/volumes/mike/20170904/openphotonet_MAJ_3303.JPG",
+    "scores": [3,3,3,3,3,3,3,3,3,3]
+};
+
+var friend = [friend1,friend2,friend3,friend4,friend5,friend6,friend7,friend8, friend9, friend10];
+
+
+module.exports = friend;
